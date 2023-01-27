016733941f37d0dcbd3d9b396d3ad98379a18481
satoshi-nakajima-uhuru <nakajima.satoshi@uhuru.jp>
2023-01-27 22:03:59 +0900

    :tada: fin section6

---
+++ section6/test/src/modules/TestComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="red-b">
+    <div class="border__blue">
+      scss のテスト
+    </div>
+    <div class="background__primary">scss のグローバルテスト</div>
+    テストです
+    {{ testData }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  data() {
+    return {
+      testData: 'テストdata'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.red-b {
+  border: 1px red solid;
+}
+.border {
+  &__blue {
+    border: 1px blue solid;
+  }
+}
+
+.background__primary {
+  background-color: $color-primary;
+}
+
+</style>
--- section6/test/vue.config.js
+++ section6/test/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '', // build 後の dist ファイルで表示するため
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "~@/assets/sass/variables.scss";`
+      }
+    }
+  }
 })
