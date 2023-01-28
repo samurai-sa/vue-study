e363a4172bdfc32a218b8020e714d67b6ac665c2
satoshi-nakajima-uhuru <nakajima.satoshi@uhuru.jp>
2023-01-28 09:19:47 +0900

    :construction: 94. 動的パラメータ+props 補足

---
+++ section7/vuerouter/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about" exact-active-class="test">About</router-link> |
+      <router-link to="/book">BookList</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-active {
+  color: red;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+nav a.test {
+  color: aqua;
+}
+</style>
---
+++ section7/vuerouter/src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import BookList from '../views/BookList.vue'
+import BookDetail from '../components/BookDetail'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/book',
+    name: 'BookList',
+    component: BookList
+  },
+  {
+    path: '/book/:id',
+    name: 'Book',
+    component: BookDetail,
+    props: (route) => ({
+      bookId: route.params.id,
+      title: route.params.title,
+      content: route.params.content,
+    })
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
