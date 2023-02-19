c41ffa5f18bae37df93b7cfda79348257b025ad6
samurai-sa <69831034+samurai-sa@users.noreply.github.com>
2023-02-19 16:54:56 +0900

    wip

---
+++ section9/vuex/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <button @click="increment">+</button>
+    <button @click="removeCount">-</button>
+    <button @click="addCount">+10</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment');
+    },
+    removeCount() {
+      this.$store.commit('decrement');
+    },
+    addCount() {
+      this.$store.commit('addCount', {
+        value: 10
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ section9/vuex/src/store/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: { // 初期値
+    count: 0
+  },
+  getters: {
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    },
+    addCount(state, payload) { // 第2引数はオブジェクト
+      state.count += payload.value;
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
